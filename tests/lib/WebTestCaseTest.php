c14bd1fe06c18bb05457752b7f405944ba4554be
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2023-05-05 10:55:39 +0200

    Added test rest

---
+++ tests/lib/WebTestCaseTest.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Test\Rest;
+
+use Ibexa\Contracts\Test\Rest\WebTestCase as BaseWebTestCase;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+
+final class WebTestCaseTest extends TestCase
+{
+    /**
+     * @dataProvider provideForTestPositiveSnapshotComparison
+     *
+     * @param "json"|"xml"|null $type
+     */
+    public function testPositiveSnapshotComparison(
+        string $snapshotContent,
+        ?string $type,
+        ?string $file = null
+    ): void {
+        $testCase = new WebTestCase();
+        $testCase->testComparison($snapshotContent, $type, $file);
+
+        self::assertCount(1, $testCase);
+    }
+
+    /**
+     * @return iterable<array{non-empty-string, "json"|"xml"|null, 2?: non-empty-string}>
+     */
+    public static function provideForTestPositiveSnapshotComparison(): iterable
+    {
+        yield [
+            <<<JSON
+                {
+                    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
+                }
+            JSON,
+            'json',
+        ];
+
+        yield [
+            <<<XML
+                <text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+                </text>
+            XML,
+            'xml',
+        ];
+    }
+
+    /**
+     * @dataProvider provideForTestNegativeSnapshotComparison
+     *
+     * @param "json"|"xml"|null $type
+     */
+    public function testNegativeSnapshotComparison(
+        string $snapshotContent,
+        ?string $type,
+        ?string $file = null,
+        ?string $expectationMessage = null
+    ): void {
+        $testCase = new WebTestCase();
+
+        try {
+            $testCase->testComparison($snapshotContent, $type, $file);
+            self::fail('This test should fail assertions.');
+        } catch (ExpectationFailedException $e) {
+            self::assertNotEmpty($expectationMessage);
+            self::assertStringContainsString(
+                $expectationMessage,
+                $e->getMessage(),
+            );
+        }
+    }
+
+    /**
+     * @return iterable<array{non-empty-string, "json"|"xml"|null, 2?: non-empty-string|null, 3?: string|null}>
+     */
+    public function provideForTestNegativeSnapshotComparison(): iterable
+    {
+        yield [
+            '{NotAProperJson}',
+            'json',
+            null,
+            "{NotAProperJson}' is valid JSON (Syntax error, malformed JSON)",
+        ];
+
+        yield [
+            '<foo />',
+            'xml',
+            __DIR__ . '/_output/different-xml.xml',
+            'Failed asserting that two DOM documents are equal',
+        ];
+
+        yield [
+            <<<JSON
+                {
+                    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
+                }
+            JSON,
+            'json',
+            __DIR__ . '/_output/different-json.json',
+            '"text": "Lorem ipsum dolor sit amet, foobar."',
+        ];
+    }
+}
+
+final class WebTestCase extends BaseWebTestCase
+{
+    /**
+     * @param "json"|"xml"|null $type
+     */
+    public function testComparison(string $snapshotContent, ?string $type, ?string $file): void
+    {
+        self::assertStringMatchesSnapshot(
+            $snapshotContent,
+            $type,
+            $file,
+        );
+    }
+}
