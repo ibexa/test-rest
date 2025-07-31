05a8d97f4cbf3915fd606645e07ed057c8829eb9
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2025-07-31 15:29:10 +0200

    Added ability to declare JSON Schema for tests

--- tests/integration/Schema/TestSchemaProvider.php
+++ tests/integration/Schema/TestSchemaProvider.php
@@ -8,36 +8,15 @@ declare(strict_types=1);
 
 namespace Ibexa\Tests\Integration\Test\Rest\Schema;
 
-use Ibexa\Contracts\Test\Rest\Schema\SchemaProviderInterface;
+use Ibexa\Contracts\Test\Rest\Schema\AbstractFileSchemaProvider;
 
-final class TestSchemaProvider implements SchemaProviderInterface
+final class TestSchemaProvider extends AbstractFileSchemaProvider
 {
-    public function provideSchemas(): iterable
-    {
-        $file = __DIR__ . '/../basic_types.json';
-        $schema = $this->decode($file);
-
-        yield 'ibexa/test-rest/basic_types' => $schema;
-    }
-
     /**
      * @throws \JsonException
      */
-    private function decode(string $file): object
+    public function provideSchemas(): iterable
     {
-        $basicTypes = file_get_contents($file);
-        if ($basicTypes === false) {
-            throw new \LogicException('Failed to load basic types schema from file: ' . $file);
-        }
-
-        $schema = json_decode($basicTypes, false, 512, JSON_THROW_ON_ERROR);
-        if (!is_object($schema)) {
-            throw new \LogicException(sprintf(
-                'Failed to decode basic types schema from file: %s. Schema is not an object.',
-                $file,
-            ));
-        }
-
-        return $schema;
+        yield 'ibexa/test-rest/basic_types' => $this->decodeFile(__DIR__ . '/../basic_types.json');
     }
 }
